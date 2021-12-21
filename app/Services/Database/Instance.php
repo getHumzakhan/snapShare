3ff77f713bab252764996d06bf5060da8aabb6b9
Hamza Khan <gethumzakhan@gmail.com>
2021-12-21 11:13:50 +0500

    added mongo cluster

--- app/Services/Database/Instance.php
+++ app/Services/Database/Instance.php
@@ -9,7 +9,7 @@ class Instance
     public $db;
     public function __construct()
     {
-        $mongo_db = new MongoDB();
+        $mongo_db = new MongoDB("mongodb+srv://snapShare:HamzaKhanPf@cluster0.mthqt.mongodb.net/snapShare?retryWrites=true&w=majority");
         $this->db = $mongo_db->snapShare;
     }
 }
